c067ee62bb40aae8e3d8ff6410b56b63ce817ed1
g15 5511 <phuonghaleduy@gmail.com>
2025-12-08 16:52:40 +0700

    Refs [TASK][#94787] [Admin] Quản lý đơn hàng

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,15 @@
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import { MainLayout } from "@/components/layout/MainLayout";
 import { RenderHome } from "@/features/home";
-import {
-  RegisterPage,
-  ActivatePage,
-  LoginPage,
-  ForgotPasswordPage,
-  ResetPasswordPage,
-  ChangePasswordPage,
-} from "@/features/auth";
-import { RenderProducts } from "@/features/product-list";
-import { RenderProductDetail } from "@/features/product-detail";
-import { ProfilePage } from "@/features/profile";
+import { RegisterPage } from "@/features/auth";
+import AdminLayout from "@/components/layout/AdminLayout";
+import AdminDashboard from "@/features/admin/dashboard";
+import AdminOrders from "@/features/admin/orders";
+import AdminProducts from "@/features/admin/products";
+import AdminCategories from "@/features/admin/categories";
+import AdminUsers from "@/features/admin/users";
+import AdminFeedback from "@/features/admin/feedback";
+
 const LayoutWrapper = () => {
   return (
     <MainLayout>
@@ -29,40 +27,21 @@ export const router = createBrowserRouter([
         element: <RenderHome />,
       },
       {
-        path: "auth/register",
+        path: "/register",
         element: <RegisterPage />,
       },
       {
-        path: "auth/activate",
-        element: <ActivatePage />,
-      },
-      {
-        path: "auth/login",
-        element: <LoginPage />,
-      },
-      {
-        path: "auth/forgot-password",
-        element: <ForgotPasswordPage />,
-      },
-      {
-        path: "auth/reset-password",
-        element: <ResetPasswordPage />,
-      },
-      {
-        path: "auth/change-password",
-        element: <ChangePasswordPage />,
-      },
-      {
-        path: "/products",
-        element: <RenderProducts />,
-      },
-      {
-        path: "/products/:id",
-        element: <RenderProductDetail />,
-      },
-      {
-        path: "/profile/:userId",
-        element: <ProfilePage />,
+        path: "/admin",
+        element: <AdminLayout />,
+        children: [
+          { path: "dashboard", element: <AdminDashboard /> },
+          { path: "orders", element: <AdminOrders /> },
+          { path: "products", element: <AdminProducts /> },
+          { path: "categories", element: <AdminCategories /> },
+          { path: "users", element: <AdminUsers /> },
+          { path: "feedback", element: <AdminFeedback /> },
+          { path: "", element: <AdminDashboard /> },
+        ],
       },
     ],
   },
